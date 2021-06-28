97caa18929875d6cdee08f1e84e11fd2ac37245c
CHENMA18 <chenma18@oocl.com>
2021-06-28 17:42:54 +0800

    [unit test]should_execute_addDeposit_of_transactionRepository_when_account_deposit in AccountTest

---
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -0,0 +1,22 @@
+package com.tw.banking;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class AccountTest {
+    @Test
+    public void should_execute_addDeposit_of_transactionRepository_when_account_deposit() {
+        //given
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        Printer printer = mock(Printer.class);
+        //when
+        Account mandy = new Account(transactionRepository, printer);
+        int amount = 999;
+        mandy.deposit(amount);
+        //then
+        verify(transactionRepository, times(1)).addDeposit(amount);
+    }
+
+}
---
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -0,0 +1,2 @@
+package com.tw.banking;public class PrinterTest {
+}

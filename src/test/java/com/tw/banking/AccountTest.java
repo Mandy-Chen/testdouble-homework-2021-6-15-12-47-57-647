a893e2960c6d29ad7be4e025d166af0cb6cf3a94
CHENMA18 <chenma18@oocl.com>
2021-06-28 17:44:26 +0800

    [unit test]should_execute_addWithdraw_of_transactionRepository_when_account_withdraw  in AccountTest

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -11,12 +11,29 @@ public class AccountTest {
         //given
         TransactionRepository transactionRepository = mock(TransactionRepository.class);
         Printer printer = mock(Printer.class);
+
         //when
         Account mandy = new Account(transactionRepository, printer);
         int amount = 999;
         mandy.deposit(amount);
+
         //then
         verify(transactionRepository, times(1)).addDeposit(amount);
+    }
+
+    @Test
+    public void should_execute_addWithdraw_of_transactionRepository_when_account_withdraw() {
+        //given
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        Printer printer = mock(Printer.class);
+
+        //when
+        Account mandy = new Account(transactionRepository, printer);
+        int amount = 999;
+        mandy.withdraw(amount);
+
+        //then
+        verify(transactionRepository, times(1)).addWithdraw(amount);
     }
 
 }

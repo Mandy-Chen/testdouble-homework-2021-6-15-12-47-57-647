6a0111759810665ef9c518fb63bc0d33dce5f186
CHENMA18 <chenma18@oocl.com>
2021-06-28 17:51:55 +0800

    [unit test]should_have_amount_when_addDeposit_given_amount  in TransactionRepositoryTest

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -1,2 +1,34 @@
-package com.tw.banking;public class TransactionRepositoryTest {
+package com.tw.banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class TransactionRepositoryTest {
+    private TransactionRepository transactionRepository;
+
+    @BeforeEach
+    public void init() {
+        Clock clock = mock(Clock.class);
+        transactionRepository = new TransactionRepository(clock);
+    }
+
+    @Test
+    public void should_have_amount_when_addDeposit_given_amount() {
+        //given
+        int amount = 999;
+        transactionRepository.addDeposit(amount);
+
+        //when
+        List<Transaction> transactions = transactionRepository.allTransactions();
+
+        //then
+        assertEquals(1, transactions.size());
+        assertEquals(999, transactions.get(0).amount());
+    }
+
 }
